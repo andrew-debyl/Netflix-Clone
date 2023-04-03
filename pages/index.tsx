81762e75a67afd7385c85a325e235a77451c13e5
Andrew <andrewdebyl@gmail.com>
2023-04-03 16:53:30 -0400

    main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Banner from "@/components/Banner";
 import requests from "@/utils/requests";
 import { Movie } from "@/typings";
+import Row from "@/components/Row";
 
 //getting the type (from typings.d.ts file) and giving it to netflixOriginals which is an array of movies
 interface Props {
@@ -28,16 +29,24 @@ const Home = ({
   documentaries,
 }: Props) => {
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
       <Head>
         <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
-      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pls-16">
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
           <Banner netflixOriginals={netflixOriginals} />
-          <section></section>
+          <section className="md:space-y-24">
+             <Row title="Trending Now" movies={trendingNow}/>
+             <Row title="Top Rated" movies={topRated}/>
+             <Row title="Action Thrillers" movies={actionMovies}/>
+             <Row title="Comedies" movies={comedyMovies}/>
+             <Row title="Horror Movies" movies={horrorMovies}/>
+             <Row title="Romance Movies" movies={romanceMovies}/>
+             <Row title="Documentaries" movies={documentaries}/>
+          </section>
       </main>
     </div>
   );
